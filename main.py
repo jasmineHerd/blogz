877e18abcf5f02972036e2d080195e6e2b4326cc
Jasmine Herd <jthm66@mail.missouri.edu>
2018-04-22 21:04:20 -0500

    database tables created

--- main.py
+++ main.py
@@ -14,11 +14,39 @@ class Blog(db.Model):
     blog_id = db.Column(db.Integer, primary_key=True)
     blog_title = db.Column(db.String(120))
     body = db.Column(db.String(400))
+    owner_id = db.Column(db.Integer,db.ForeignKey('user.id'))
     
-    def __init__(self, blog_title, body):
+    def __init__(self, blog_title, body,owner):
         self.blog_title = blog_title
         self.body = body
-        
+        self.owner = owner
+################
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+
+
+
+
+
+
+
+
+
+
+
+###################
 @app.route('/')
 def home():
     return redirect('/blog')     
